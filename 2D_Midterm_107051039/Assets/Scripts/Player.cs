754e3df581d9a71ce34589bb8db3ae9a5d80f63b
yuievon0ee <71679885+yuievon0ee@users.noreply.github.com>
2020-11-14 16:57:52 +0800

    差音樂音效和跳

---
+++ 2D_Midterm_107051039/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    public Text textCount;
+    public int count;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "終點" )
+        {
+            final.SetActive(true);
+        }
+        if (collision.tag == "櫻桃")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "X" + count;
+        }
+    }
+}
+
+
